1c7677c31a79f72a1833276a2cba687bc1fba858
Rebecca2419 <139363310+Rebecca2419@users.noreply.github.com>
2024-03-31 20:16:29 +0800

    Add files via upload

---
+++ test_vgg_caltech.py
@@ -0,0 +1,47 @@
+import os
+
+#change the path for different data
+path="./images/test_caltech_32"
+
+# 返回path下所有文件构成的一个list列表
+filelist=os.listdir(path)
+
+correct=0
+total = 0
+
+# 遍历输出每一个文件的名字和类型
+for item in filelist:
+    # 输出指定后缀类型的文件
+    if(item.endswith('.png')):
+        total += 1
+        im_id = item.split('.p')
+        class_id = im_id[0].split('_')
+        image=class_id[1] #use class name as the file name
+        
+        os.system("touch "+ image + ".txt")
+        cmd="timeout 0.7 ./build/install/bin/tm_classification -t 8 -s 0.01712475383,0.0175070028,0.0174291939 -w 123.675,117.3,103.53 \
+            -g 32,32 -m models_convert/VGG16_caltech101.tmfile -i images/test_caltech_32/" + item + " >&" + image + ".txt"
+        
+        os.system(cmd)
+        
+        #p = subprocess.Popen(cmd, shell = True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        #stdout,stderr = p.communicate()
+        #print('stdout : ',stdout)
+        #print('stderr : ',stderr)
+
+	    #读image.txt，if 正确 correct+=1
+        f = open(image + ".txt", 'r')
+        lines = f.readlines()
+        result = lines[7].split(", ")
+        my_class = result[1]
+        
+        if (my_class == (image+'\n')):
+            correct += 1
+
+        f.close()
+        os.remove(image + ".txt")
+        
+        accuracy = correct/total
+        print('image: ', total, f"Accuracy: {accuracy:.4f}")	
+
+print("End here")
---
+++ test_vgg_svhn.py
@@ -0,0 +1,48 @@
+import os
+
+#change the path for different data
+path="./images/test_SVHN_32"
+
+# 返回path下所有文件构成的一个list列表
+filelist=os.listdir(path)
+
+correct=0
+total = 0
+
+# 遍历输出每一个文件的名字和类型
+for item in filelist:
+    # 输出指定后缀类型的文件
+    if(item.endswith('.png')):
+        total += 1
+        im_id = item.split('.p')
+        class_id = im_id[0].split('_')
+        image=class_id[1] #use class name as the file name
+        
+        os.system("touch "+ image + ".txt")
+        cmd="timeout 0.7 ./build/install/bin/tm_classification -t 8 -s 0.01938492,0.01966684,0.01951029 -w 125.307,122.961,113.8575\
+            -g 32,32 -m models_convert/VGG16_SVHN.tmfile -i images/test_SVHN_32/" + item + " >&" + image + ".txt"
+        
+        os.system(cmd)
+        
+        #p = subprocess.Popen(cmd, shell = True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        #stdout,stderr = p.communicate()
+        #print('stdout : ',stdout)
+        #print('stderr : ',stderr)
+
+	    #读image.txt，if 正确 correct+=1
+        f = open(image + ".txt", 'r')
+        lines = f.readlines()
+        result = lines[7].split(", ")
+        my_class = result[1]
+        
+        if (my_class == image+'\n'):
+            correct += 1
+
+        f.close()
+        os.remove(image + ".txt")
+        
+        accuracy = correct/total
+        print('image: ', total, f"Accuracy: {accuracy:.4f}")
+        
+        
+print("End here")
